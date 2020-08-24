dbb638d06d23f9ce9fc28aac5d477b464b66b536
MainaGeorge <nuriszka@gmail.com>
2020-08-24 08:42:25 +0200

    including tests to the data structures

---
+++ StructureTests/GenericStackImplementationTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using dojo;
+using Xunit;
+
+namespace StructuresTests
+{
+    public class GenericStackImplementationTests
+    {
+        [Fact]
+        public void StackStartsOffEmpty()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>();
+
+            Assert.Equal(0, stack.Count);
+        }
+
+        [Fact]
+        public void StackStartsOffWithOneElement()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>(new Node<string>("home"));
+
+            Assert.Equal(1, stack.Count);
+            Assert.Equal("home", stack.Peek.Value);
+        }
+
+        [Fact]
+        public void StackCanPushElements()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>(new Node<string>("home"));
+
+            stack.Push("neighbor");
+            stack.Push("lovely");
+
+            Assert.Equal(3, stack.Count);
+        }
+
+        [Fact]
+        public void StackCanPeekInToSeeNextElement()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>(new Node<string>("home"));
+
+            stack.Push("added");
+
+            Assert.Equal("added", stack.Peek.Value);
+        }
+
+        [Fact]
+        public void StackPopsOffTheCorrectElement()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>(new Node<string>("home"));
+
+            stack.Push("name");
+            stack.Push("email");
+
+            var popped = stack.Pop.Value;
+
+            Assert.Equal("email", popped);
+        }
+
+        [Fact]
+        public void StackReturnsNullWhilePeekingEmptyStack()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>();
+
+            Assert.Null(stack.Peek);
+        }
+
+        [Fact]
+        public void StackThrowsArgumentExceptionWhilePoppingEmptyStack()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>();
+
+            Assert.Throws<ArgumentException>(() => stack.Pop);
+        }
+    }
+}
