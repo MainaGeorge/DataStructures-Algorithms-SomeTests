b9f9a689d63398fbd29230c1cc1f96979e1adce7
MainaGeorge <nuriszka@gmail.com>
2020-08-24 08:54:44 +0200

    recreating the tests

---
+++ DataStructureTests/GenericLinkedListTests.cs
@@ -0,0 +1,145 @@
+using System;
+using dojo;
+using Xunit;
+
+namespace DataStructureTests
+{
+    public class GenericLinkedListTests
+    {
+        [Fact]
+        public void ListStartsOffEmpty()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            Assert.Equal(0, linkedList.Length);
+        }
+
+        [Fact]
+        public void ListStartsWithAHeadNode()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>(new Node<string>("father"));
+
+            Assert.Equal(1, linkedList.Length);
+            Assert.Equal("father", linkedList.GetFirst.Value);
+        }
+
+        [Fact]
+        public void ListAddElementsCorrectly()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+
+            Assert.Equal(3, linkedList.Length);
+        }
+
+        [Fact]
+        public void ListAddsElementsAtTheBeginning()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+            linkedList.AddFirst("sister");
+
+            Assert.Equal("sister", linkedList.GetFirst.Value);
+        }
+
+        [Fact]
+        public void ListAddsElementAtTheEnd()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+
+            linkedList.AddLast("last element");
+
+            Assert.Equal("last element", linkedList.GetLast.Value);
+        }
+
+        [Fact]
+        public void ListRemovesFirstElement()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+
+            linkedList.RemoveFirst();
+            Assert.Equal("father", linkedList.GetFirst.Value);
+        }
+
+        [Fact]
+        public void ListRemovesLastElement()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+
+            linkedList.RemoveLast();
+
+            Assert.Equal("father", linkedList.GetLast.Value);
+
+        }
+
+        [Fact]
+        public void ListRemovesAtASpecificIndex()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+
+            linkedList.RemoveAt(1);
+
+            Assert.Equal(2, linkedList.Length);
+            Assert.Equal(linkedList.GetLast.Value, linkedList.GetAtIndex(1).Value);
+        }
+
+        [Fact]
+        public void ListAddsAtASpecificIndex()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+
+            linkedList.AddAt(1, "adding at 1");
+
+            Assert.Equal("adding at 1", linkedList.GetAtIndex(1).Value);
+        }
+
+        [Fact]
+        public void ListThrowsExceptionIfIndexOutOfRangeWhenRemoving()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+
+            Assert.Throws<IndexOutOfRangeException>(() => linkedList.RemoveAt(linkedList.Length + 1));
+        }
+        [Fact]
+        public void ListThrowsExceptionIfIndexOutOfRangeWhenAdding()
+        {
+            var linkedList = new GenericLinkedListImplementation<string>();
+
+            linkedList.AddNode("mother");
+            linkedList.AddNode("father");
+            linkedList.AddNode("son");
+
+            Assert.Throws<IndexOutOfRangeException>(() => linkedList.AddAt(linkedList.Length + 1, "daughter"));
+        }
+    }
+}
---
+++ DataStructureTests/GenericStackImplementationTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using dojo;
+using Xunit;
+
+namespace DataStructureTests
+{
+    public class GenericStackImplementationTests
+    {
+        [Fact]
+        public void StackStartsOffEmpty()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>();
+
+            Assert.Equal(0, stack.Count);
+        }
+
+        [Fact]
+        public void StackStartsOffWithOneElement()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>(new Node<string>("home"));
+
+            Assert.Equal(1, stack.Count);
+            Assert.Equal("home", stack.Peek.Value);
+        }
+
+        [Fact]
+        public void StackCanPushElements()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>(new Node<string>("home"));
+
+            stack.Push("neighbor");
+            stack.Push("lovely");
+
+            Assert.Equal(3, stack.Count);
+        }
+
+        [Fact]
+        public void StackCanPeekInToSeeNextElement()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>(new Node<string>("home"));
+
+            stack.Push("added");
+
+            Assert.Equal("added", stack.Peek.Value);
+        }
+
+        [Fact]
+        public void StackPopsOffTheCorrectElement()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>(new Node<string>("home"));
+
+            stack.Push("name");
+            stack.Push("email");
+
+            var popped = stack.Pop.Value;
+
+            Assert.Equal("email", popped);
+        }
+
+        [Fact]
+        public void StackReturnsNullWhilePeekingEmptyStack()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>();
+
+            Assert.Null(stack.Peek);
+        }
+
+        [Fact]
+        public void StackThrowsArgumentExceptionWhilePoppingEmptyStack()
+        {
+            var stack = new GenericStackFromLinkedListImplementation<string>();
+
+            Assert.Throws<ArgumentException>(() => stack.Pop);
+        }
+    }
+}
